34a23b3ddf863379fc65a43ebf7ff0e3bd496b1e
nayan-bansal <nayan.7991@gmail.com>
2020-09-24 13:30:56 +0530

    Updated: Monthly Wage Calculation

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,12 @@
 package javaEmployee;
 
-public class UC_4 {
-
+public class UC_5 {
 	public static final int  EMP_FTE = 1;
 	public static final int  EMP_WAGE=20;	
 	public static final int  EMP_HOUR=8;
 	public static final int  EMP_PTE = 2;
 	public static final int  EMP_PTE_HOUR = 4;
+	public static final int  EMP_WORKING_DAYS = 20;
 	
 		public static void main(String[] args) {
 		
@@ -14,25 +14,21 @@ public class UC_4 {
 			System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 			
 			//Data Type to check Attendance
-			int check = (int) Math.floor(Math.random()*10)%3;
+			double check = Math.floor(Math.random()*10)%3;
 			System.out.println("Employ Check: "+ check);
 			
 			//Main Logic of Program
-			switch (check) {
-			
-			case EMP_FTE :  
+			if(check == EMP_FTE) { 
 			System.out.println("Employee is Present");	
-			System.out.println("Employ Wage= "+EMP_WAGE*EMP_HOUR);
-			break;
-			
-			case EMP_PTE :  
-			System.out.println("Employee is Present and Part Time");	
-			System.out.println("Employ Wage= "+EMP_WAGE*EMP_PTE_HOUR);
-			break;
-				
-			default :
-			System.out.println("Employee is Absent");	
+			System.out.println("Employ Wage= "+EMP_WAGE*EMP_HOUR*EMP_WORKING_DAYS);
 			}
+			else if(check == EMP_PTE) { 
+				System.out.println("Employee is Present and Part Time");	
+				System.out.println("Employ Wage= "+EMP_WAGE*EMP_PTE_HOUR*EMP_WORKING_DAYS);
+				}
+			else
+			System.out.println("Employee is Absent");	
+		
 		}
 
 }
