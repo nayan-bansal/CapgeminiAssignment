adca2b3bf2ba3aa6157d8a65d2837b0131623d19
nayan-bansal <nayan.7991@gmail.com>
2020-09-24 13:12:23 +0530

    Welcome Message Added

---
+++ EmployeeWage.java
@@ -0,0 +1,13 @@
+package javaEmployee;
+
+public class UC_0 {
+
+	public static void main(String[] args) {
+		
+		//Print Starting Message
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		
+			
+	
+	}
+}
