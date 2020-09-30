c0da517883df0f7ef5dd812b55482180d23961d2
nayan-bansal <nayan.7991@gmail.com>
2020-09-30 05:50:31 +0530

    Added: Employee Wage using class Multiple Companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,57 +1,54 @@
 package javaEmployee;
 
 public class EmployeeWage {
-//Constant
+//constant
 	public static final int IS_FULL_TIME =1;
 	public static final int IS_PART_TIME=2;
-	public static final int WAGE_PER_HOUR=20;
-	public static final int TOTAL_WORKING_HRS=100;
-	public static final int TOTAL_WORKING_DAYS=20;
 	
-	//method to compute the employee wages
-	public static int empWageCalculation(int emp_check) {
-		int emp_hours=0;
-		int emp_wage=0;
-		
+	//Variables
+	private int working_hrs_per_month;
+	private int emp_wage_per_hr;
+	private int working_day_per_month;
+	private int emp_hrs_per_day;
+	private String company_name;
+	
+	public EmpWage(int emp_wage_per_hr, int working_day_per_month, int working_hrs_per_month, String company_name) {
+		this.company_name=company_name;
+		this.emp_wage_per_hr=emp_wage_per_hr;
+		this.working_hrs_per_month=working_hrs_per_month;
+		this.working_day_per_month=working_day_per_month;
+	}
+	
+	public int CompanyWage() {
+		int total_hrs=0;
+		int total_working_days=0;
+		System.out.println("Day\t Hours Worked\tTotal Wage");
+		while(total_hrs<=working_hrs_per_month && total_working_days<working_day_per_month) {
+		total_working_days++;
+		int emp_check= (int) Math.floor(Math.random()*10)%3;
 		switch(emp_check) {
-		case IS_FULL_TIME :
-			System.out.println("Employee is full time");
-			emp_hours=8;
-			if(emp_hours*TOTAL_WORKING_DAYS>=TOTAL_WORKING_HRS) {
-				emp_wage=TOTAL_WORKING_HRS*WAGE_PER_HOUR;
-			}
-			else {
-				emp_wage=TOTAL_WORKING_DAYS*emp_hours*WAGE_PER_HOUR;
-			}
+		case IS_FULL_TIME:
+			emp_hrs_per_day=8;
 			break;
-		case IS_PART_TIME : 
-			System.out.println("Employee is part time");
-			emp_hours=4;
-			if(emp_hours*TOTAL_WORKING_DAYS>=TOTAL_WORKING_HRS) {
-				emp_wage=TOTAL_WORKING_HRS*WAGE_PER_HOUR;
-			}
-			else {
-				emp_wage=TOTAL_WORKING_DAYS*emp_hours*WAGE_PER_HOUR;
-			}
+		case IS_PART_TIME:
+			emp_hrs_per_day=4;
 			break;
-		default :
-			System.out.println("Employee absent");
-			emp_wage=0;
+		default:  
+			emp_hrs_per_day=0;
+			
 		}
-		
-		return emp_wage;
+		int emp_wage=emp_hrs_per_day*emp_wage_per_hr;
+		total_hrs+=emp_hrs_per_day;
+		System.out.println(" "+total_working_days+" \t"+total_hrs+" \t\t"+emp_wage);
+	}
+		return total_hrs*emp_wage_per_hr;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-		
-		//Generating random number to determine type of object
-		int type_of_emp = (int) Math.floor(Math.random()*10)%3;
-		
-		//Calling the static method which computes employee wage
-		int emp_wage=empwage.empWageCalculation(type_of_emp);
-		System.out.println("Employee Wage for a month: "+ emp_wage);
-		
+		EmpWage capgemini=new EmpWage(50,20,100,"Capgemini");
+		EmpWage IBM=new EmpWage(40,28,90,"IBM");
+		System.out.println("Total Employee wage for Capgemini comapny is "+capgemini.CompanyWage());
+		System.out.println("Total Employee wage for IBM comapny is "+IBM.CompanyWage());
 	}
-
+	
 }
