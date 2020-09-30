59bd306eb1e0c612997e4be7520684c84564a0f8
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 00:50:32 +0530

    Added: Interface for Multiple Companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -2,24 +2,12 @@ package javaEmployee;
 
 public class EmployeeWageComputation{
 	
-	private static final int IS_ABSENT = 0;
-	private static final int IS_PART_TIME = 1;
-	private static final int IS_FULL_TIME = 2;
-	
-	private static final int IS_AMAZON = 0;
-	private static final int IS_RELIANCE = 1;
-	private static final int IS_CAPGEMINI = 2;
-
-	
 	public static void main(String[] args) {
 	
+		
 		int emp_check = (int) (Math.floor(Math.random()*10)%3);
 		int company_check = (int) (Math.floor(Math.random()*10)%3);
-		
-		int emp_wage = 0;
-		
-		
-		
+	
 		switch(company_check) {
 		
 		case 1: //Reliance
