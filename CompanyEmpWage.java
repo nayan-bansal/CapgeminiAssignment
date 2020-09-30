e7f4ba06d917877a3a794b48b593ef92a780bb76
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 01:07:04 +0530

    Added: ArrayList for Multiple Companies

---
+++ CompanyEmpWage.java
@@ -0,0 +1,40 @@
+package javaemployee;
+
+public class CompanyEmpWage {
+	private int working_hrs_per_month;
+	private int emp_wage_per_hr;
+	private int working_day_per_month;
+	private String company_name;
+
+	public int getWorking_hrs_per_month() {
+		return working_hrs_per_month;
+	}
+
+	public void setWorking_hrs_per_month(int working_hrs_per_month) {
+		this.working_hrs_per_month = working_hrs_per_month;
+	}
+
+	public int getEmp_wage_per_hr() {
+		return emp_wage_per_hr;
+	}
+
+	public void setEmp_wage_per_hr(int emp_wage_per_hr) {
+		this.emp_wage_per_hr = emp_wage_per_hr;
+	}
+
+	public int getWorking_day_per_month() {
+		return working_day_per_month;
+	}
+
+	public void setWorking_day_per_month(int working_day_per_month) {
+		this.working_day_per_month = working_day_per_month;
+	}
+
+	public String getCompany_name() {
+		return company_name;
+	}
+
+	public void setCompany_name(String company_name) {
+		this.company_name = company_name;
+	}
+}
