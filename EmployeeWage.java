ad28bfb30449b79fd253238799f7a1db62701575
nayan-bansal <nayan.7991@gmail.com>
2020-10-01 01:06:29 +0530

    Added: ArrayList for Multiple Companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,60 +1,61 @@
 package javaEmployee;
 
-interface Employee{
+import java.util.*;
 
-	public	void EmpWage();
+public class EmployeeWage {
+	//constant
+	public static final int IS_FULL_TIME =1;
+	public static final int IS_PART_TIME=2;
+	Scanner sc=new Scanner(System.in);
 	
-}
-
-public class EmployeeWage implements Employee {
+	//ArrayList
+	public ArrayList<CompanyEmpWage> company_emp_wage=new ArrayList<CompanyEmpWage>();
 	
-	int emp_wage = 0;
-	
-	private int emp_check;
-	private int wage_hour;
-	private int hours_FTE;
-	private int hour_limit;
-	private int month_days;
-	private int hours_PTE;
-	
-	int emp_hour = 0;
-	int day_count =0;
-	int total_wage = 0;
-
-	
-	public EmployeeWage(int emp_check, int wage_hour ,int hours_FTE ,int hours_PTE, int hour_limit, int month_days) {
-		
-		this.emp_check  = emp_check;
-		this.wage_hour = wage_hour;
-		this.hours_FTE = hours_FTE;
-		this.hours_PTE = hours_PTE;
-		this.hour_limit = hour_limit;
-		this.month_days = month_days;
+	//setter
+	public void setCompany(ArrayList<CompanyEmpWage> company_emp_wage) {
+		this.company_emp_wage=company_emp_wage;
 	}
 	
-	@Override
-	public void EmpWage() {
-		
-		System.out.println("Days \t Total Hours \t Total Wage");
-		while(emp_hour<hour_limit && day_count<=month_days) {
+	//getter
+	public ArrayList<CompanyEmpWage> getCompany() {
+		return company_emp_wage;
+	}
+	
+	//Method to add contact person details
+	public void addCompany(CompanyEmpWage pobj ) {
+		company_emp_wage.add(pobj);
+	}
+	
+	//method to view company employee details
+	public ArrayList<CompanyEmpWage> viewAllDetails(){
+		return company_emp_wage;
+	}	
+	
+	//Method to compute Company Wage
+	public int computeEmpWage(int emp_wage_per_hr, int working_day_per_month, int working_hrs_per_month, String company_name) {
+		int emp_hrs_per_day;
+		int total_hrs=0;
+		int total_working_days=0;
+		System.out.println("Day\t Hours Worked\tTotal Wage");
+		while(total_hrs<=working_hrs_per_month && total_working_days<working_day_per_month) {
+		total_working_days++;
+		int emp_check= (int) Math.floor(Math.random()*10)%3;
+		switch(emp_check) {
+		case IS_FULL_TIME:
+			emp_hrs_per_day=8;
+			break;
+		case IS_PART_TIME:
+			emp_hrs_per_day=4;
+			break;
+		default:  
+			emp_hrs_per_day=0;
 			
-			switch(emp_check) {
-			case 1: 
-			total_wage = total_wage + wage_hour*hours_PTE;
-			emp_hour = emp_hour + hours_PTE;
-			System.out.println(day_count+" \t "+emp_hour+" \t "+total_wage+" (Part Time Employee)");
-			break;
-			case 2: 
-			total_wage = total_wage + wage_hour*hours_FTE;
-			emp_hour = emp_hour + hours_FTE;
-			System.out.println(day_count+" \t "+emp_hour+" \t \t"+total_wage+" (Full Time Employee)");
-			break;
-			case 0:
-			System.out.println("Employee is absent on Day "+day_count);
-			break;
-			}
-			++day_count;
 		}
-		System.out.println("Finally Wage calculated for one month is : "+total_wage);
+		int emp_wage=emp_hrs_per_day*emp_wage_per_hr;
+		total_hrs+=emp_hrs_per_day;
+		total_working_days++;
+		System.out.println(" "+total_working_days+" \t"+total_hrs+" \t\t"+emp_wage);
 	}
-}
+		return total_hrs*emp_wage_per_hr;
+	}
+	}
