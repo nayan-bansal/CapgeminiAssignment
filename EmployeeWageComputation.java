fb66af1bfc01723ed0b2e330a41a14e745599eea
nayan-bansal <nayan.7991@gmail.com>
2020-09-30 11:45:55 +0530

    Added: Emp Wage using EmpWageBuilder multiple companies

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -18,10 +18,27 @@ public class EmployeeWageComputation{
 		
 		int emp_wage = 0;
 		
-		 // Capgemini
-		System.out.println("Capgemini Employee");
-		EmployeeWage wageCapgemini = new EmployeeWage(emp_check, 55, 10, 4, 160, 20);
-                wageCapgemini.EmpWage();
 		
+		
+		switch(company_check) {
+		
+		case 1: //Reliance
+			System.out.println("Reliance Employee");
+			EmployeeWage wageReliance = new EmployeeWage(emp_check, 45, 9, 6, 150, 18);
+			 wageReliance.EmpWage();	
+			break;
+		
+		case 2: // Capgemini
+			System.out.println("Capgemini Employee");
+			EmployeeWage wageCapgemini = new EmployeeWage(emp_check, 55, 10, 4, 160, 20);
+		     wageCapgemini.EmpWage();
+			break;
+		
+		case 0: // Amazon 
+			System.out.println("Amazon Employee");
+			EmployeeWage wageAmazon = new EmployeeWage(emp_check, 50, 12, 5, 180, 22);
+		     wageAmazon.EmpWage();
+			break;
+		}
 	}
 }
