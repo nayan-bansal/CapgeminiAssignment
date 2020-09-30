ca471364ef4bb8ac3c4c9a6387553ad964d3b78a
nayan-bansal <nayan.7991@gmail.com>
2020-09-30 11:40:55 +0530

    Added: Emp Wage using EmpWageBuilder single company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,54 +1,58 @@
 package javaEmployee;
 
 public class EmployeeWage {
-//constant
-	public static final int IS_FULL_TIME =1;
-	public static final int IS_PART_TIME=2;
 	
-	//Variables
-	private int working_hrs_per_month;
-	private int emp_wage_per_hr;
-	private int working_day_per_month;
-	private int emp_hrs_per_day;
-	private String company_name;
+	int emp_wage = 0;
 	
-	public EmpWage(int emp_wage_per_hr, int working_day_per_month, int working_hrs_per_month, String company_name) {
-		this.company_name=company_name;
-		this.emp_wage_per_hr=emp_wage_per_hr;
-		this.working_hrs_per_month=working_hrs_per_month;
-		this.working_day_per_month=working_day_per_month;
+	private int emp_check;
+	private int wage_hour;
+	private int hours_FTE;
+	private int hour_limit;
+	private int month_days;
+	
+	int emp_hour = 0;
+	int day_count =0;
+	int total_wage = 0;
+
+	private int hours_PTE;
+	
+	public EmployeeWage(int emp_check, int wage_hour ,int hours_FTE ,int hours_PTE, int hour_limit, int month_days) {
+		
+		this.emp_check  = emp_check;
+		this.wage_hour = wage_hour;
+		this.hours_FTE = hours_FTE;
+		this.hours_PTE = hours_PTE;
+		this.hour_limit = hour_limit;
+		this.month_days = month_days;
 	}
 	
-	public int CompanyWage() {
-		int total_hrs=0;
-		int total_working_days=0;
-		System.out.println("Day\t Hours Worked\tTotal Wage");
-		while(total_hrs<=working_hrs_per_month && total_working_days<working_day_per_month) {
-		total_working_days++;
-		int emp_check= (int) Math.floor(Math.random()*10)%3;
-		switch(emp_check) {
-		case IS_FULL_TIME:
-			emp_hrs_per_day=8;
-			break;
-		case IS_PART_TIME:
-			emp_hrs_per_day=4;
-			break;
-		default:  
-			emp_hrs_per_day=0;
+	
+	public void EmpWage() {
+		
+		System.out.println("Days \t Total Hours \t Total Wage \t Capgemini");
+		while(emp_hour<hour_limit && day_count<=month_days) {
 			
+			switch(emp_check) {
+			case 1: 
+			total_wage = total_wage + wage_hour*hours_PTE;
+			emp_hour = emp_hour + hours_PTE;
+			System.out.println(day_count+" \t "+emp_hour+" \t "+total_wage+" (Part Time Employee)");
+			break;
+			case 2: 
+			total_wage = total_wage + wage_hour*hours_FTE;
+			emp_hour = emp_hour + hours_FTE;
+			System.out.println(day_count+" \t "+emp_hour+" \t \t"+total_wage+" (Full Time Employee)");
+			break;
+			case 0:
+			System.out.println("Employee is absent on Day "+day_count);
+			break;
+			}
+			++day_count;
 		}
-		int emp_wage=emp_hrs_per_day*emp_wage_per_hr;
-		total_hrs+=emp_hrs_per_day;
-		System.out.println(" "+total_working_days+" \t"+total_hrs+" \t\t"+emp_wage);
+		System.out.println("Finally Wage calculated for one month is : "+total_wage);
 	}
-		return total_hrs*emp_wage_per_hr;
-	}
+
+
 	
-	public static void main(String[] args) {
-		EmpWage capgemini=new EmpWage(50,20,100,"Capgemini");
-		EmpWage IBM=new EmpWage(40,28,90,"IBM");
-		System.out.println("Total Employee wage for Capgemini comapny is "+capgemini.CompanyWage());
-		System.out.println("Total Employee wage for IBM comapny is "+IBM.CompanyWage());
-	}
 	
 }
